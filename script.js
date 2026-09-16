75315c68415a0fdb8c4b13eb501c1001a1228260
Andre303q <graciabonillasebastianandre@gmail.com>
2026-09-16 09:18:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,24 @@ const productosEjemplo = [
   { id: "2", nombre: "Camiseta Casual Estampada Minimal", precio: 135.00, tipo: "ropa", imagen: "https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?auto=format&fit=crop&w=600&q=80" },
   { id: "3", nombre: "Pantalón Cargo Urbano Beige", precio: 295.00, tipo: "ropa", imagen: "https://images.unsplash.com/photo-1624378439575-d8705ad7ae80?auto=format&fit=crop&w=600&q=80" },
   { id: "4", nombre: "Sudadera Manga Larga Estilo Urbano", precio: 250.00, tipo: "ropa", imagen: "https://images.unsplash.com/photo-1556905055-8f358a7a47b2?auto=format&fit=crop&w=600&q=80" },
+  { id: "5", nombre: "Pantalon Gris Estilo y2k", precio: 265.00, tipo: "ropa", imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ64HzNIcTADawH1w6YY0B3Qt6ax8fI-AbwoWd0pCCCqg&s=10" },
+  { id: "6", nombre: "Pantalon Cargo Mujer Azul", precio: 250.00, tipo: "ropa", imagen: "https://m.media-amazon.com/images/I/812w-hkANhL._AC_SY1000_.jpg" },
   
   // Accesorios / Relojes (Individuales sin selector)
-  { id: "5", nombre: "Reloj Minimalista Cronógrafo Acero", precio: 480.00, tipo: "reloj", imagen: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?auto=format&fit=crop&w=600&q=80" },
-  { id: "6", nombre: "Reloj Deportivo Smartwatch Pro", precio: 699.00, tipo: "reloj", imagen: "https://images.unsplash.com/photo-1508685096489-7aacd43bd3b1?auto=format&fit=crop&w=600&q=80" },
-  
-  // Coleccionables y Juguetes (Individuales sin selector)
-  { id: "7", nombre: "Figura Coleccionable de Anime Edición Especial", precio: 320.00, tipo: "coleccionable", imagen: "https://images.unsplash.com/photo-1607604276583-eef5d076aa5f?auto=format&fit=crop&w=600&q=80" },
-  { id: "8", nombre: "Auto de Colección Escala Clásico", precio: 150.00, tipo: "coleccionable", imagen: "https://images.unsplash.com/photo-1594787318286-3d835c1d207f?auto=format&fit=crop&w=600&q=80" }
+  { id: "7", nombre: "Reloj Minimalista Cronógrafo Acero", precio: 480.00, tipo: "reloj", imagen: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?auto=format&fit=crop&w=600&q=80" },
+  { id: "8", nombre: "Reloj Deportivo Smartwatch Pro", precio: 699.00, tipo: "reloj", imagen: "https://images.unsplash.com/photo-1508685096489-7aacd43bd3b1?auto=format&fit=crop&w=600&q=80" },
+  { id: "9", nombre: "Cadena para Pantalon", precio: 129.00, tipo: "accesorio", imagen: "https://preview.redd.it/where-to-find-clothing-accessories-that-fit-grunge-y2k-easy-v0-532xvpj1x2cf1.jpg?width=640&crop=smart&auto=webp&s=59263934be3db1987893582837e12e9734468937" },
+  { id: "10", nombre: "Pulsera estilo y2k", precio: 129.00, tipo: "accesorio", imagen: "https://i.etsystatic.com/45963821/r/il/884f55/6146331475/il_fullxfull.6146331475_iv9b.jpg" },
+  { id: "11", nombre: "Muñequera punk", precio: 219.00, tipo: "accesorio", imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIZ9XXOQa9jGzpeF5VdUxm7kQMai7ao_auOu9FcglCJJe2-vCPGGGhZBo&s=10" },
+  { id: "12", nombre: "Pendientes Estilo Fresa", precio: 110.00, tipo: "accesorio", imagen: "https://www.chocolatecomplementos.com/wp-content/uploads/2026/02/fresa-arito-300x300.jpg" },
+ 
+  // Coleccionables / Juguetes / Posters / etc (Individuales sin selector)
+  { id: "13", nombre: "Posters estilo Anime", precio: 320.00, tipo: "coleccionable", imagen: "https://images.unsplash.com/photo-1607604276583-eef5d076aa5f?auto=format&fit=crop&w=600&q=80" },
+  { id: "14", nombre: "Auto de Colección Escala Clásico", precio: 150.00, tipo: "coleccionable", imagen: "https://images.unsplash.com/photo-1594787318286-3d835c1d207f?auto=format&fit=crop&w=600&q=80" },
+  { id: "15", nombre: "Coleccionable de Pokemon Bulbasur", precio: 299.00, tipo: "coleccionable", imagen: "https://media.vulcanita.com.ar/uploads/2024/11/Foto1_Pokemon_PKW2297_Pack-.webp" },
+  { id: "16", nombre: "Poster Serie Stranger Things", precio: 129.00, tipo: "coleccionable", imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdJrOB93U48zYkrjwNa78MV_hjW0V8_ieOuozrxGUfLW51YU2itbjptTI&s=10" },
+  { id: "17", nombre: "Poster Rocky The Movie", precio: 149.00, tipo: "coleccionable", imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjXoPXyPxVf8rQ3n2ZBSBvALvEoC67ebI79uvW9KcETA&s=10" },
+  { id: "18", nombre: "Poster Star Wars", precio: 149.00, tipo: "coleccionable", imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVQHjwyS0oHXHk2SqGFuBCJ6CGAMuAg4fdBUvJO_sXIdTHfcT0xihO_RQ&s=10" }
 ];
 
 let carrito = [];
